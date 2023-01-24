76ee313f5134639ec8b042dd9b9b1947928c6e32
Szymon Jakubowski <sj.kaniasiatka@gmail.com>
2023-01-24 16:32:38 +0100

    List to do commit

---
+++ script.js
@@ -0,0 +1,32 @@
+{
+
+    const tasks = [
+        {
+            content: "nagrac lekcje"
+        },
+        {
+            content: " zjeść obiad"
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+        <li>
+        ${task.content}
+        </li>
+        `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
