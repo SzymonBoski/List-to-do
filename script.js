4895c87afb46164dd602cc8fb8c9f837b553a697
Szymon Jakubowski <sj.kaniasiatka@gmail.com>
2023-01-25 16:00:53 +0100

    binding events moved to a separate function

--- script.js
+++ script.js
@@ -27,6 +27,24 @@
         render();
     };
 
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButtons, index) => {
+            toggleDoneButtons.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -43,21 +61,7 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            });
-        });
-
-        const toggleDoneButtons = document.querySelectorAll(".js-done");
-
-        toggleDoneButtons.forEach((toggleDoneButtons, index) => {
-            toggleDoneButtons.addEventListener("click", () => {
-                toggleTaskDone(index);
-            });
-        });
+       bindEvents();
     };
 
 
